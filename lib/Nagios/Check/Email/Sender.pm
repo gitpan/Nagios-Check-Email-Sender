3d1fb3807cebef8356f2bbcdd8e01e2b8535c377
Marc Chantreux <marcc@cpan.org>
2013-06-05 14:45:34 +0000

    Import of MARCC/Nagios-Check-Email-Sender-0.1 from CPAN.
    gitpan-cpan-distribution: Nagios-Check-Email-Sender
    gitpan-cpan-version:      0.1
    gitpan-cpan-path:         MARCC/Nagios-Check-Email-Sender-0.1.tar.gz
    gitpan-cpan-author:       MARCC
    gitpan-cpan-maturity:     released

--- lib/Nagios/Check/Email/Sender.pm
+++ lib/Nagios/Check/Email/Sender.pm
@@ -2,7 +2,7 @@ package Nagios::Check::Email::Sender;
 use strict;
 use warnings;
 
-our $VERSION = '0.0';
+our $VERSION = '0.1';
 # ABSTRACT: nagios check for complete email sending
 
 =head1 SYNOPSIS 
@@ -17,13 +17,16 @@ just run
 
 when conf.yml can be 
 
-
     host: mailhost.example.com
     port: 25 
     to: monitor-recipient@example.com
     from: monitor@example.com 
 
-I really think that every parameter are self-explained. please email me if not.
+I really think that every parameter are self-explained. please email me if not. 
+
+You can also pass every param from argv (Thonas said it's a feature):
+
+    ./check_email_sender.pl host mailhost.example.com port 25 to monitor-recipient@example.com from monitor@example.com 
 
 =cut
 
